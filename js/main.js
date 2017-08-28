9bc0e73b853cf0a0a22e0cd87ec76becc6ef8355
Anand <anand.pal2050@gmail.com>
2017-08-28 21:00:40 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -65,3 +65,12 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// Smooth Scroll
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+});
